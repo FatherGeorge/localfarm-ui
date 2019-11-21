03aaf418191c5c0fafa8edb20c6401794409da5d
fathergeorge <vovathewombat@yandex.ru>
2019-11-21 20:18:12 +0530

    move to components

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className="Header">Header</div>
+      <div className="Search">Search</div>
+      <div className="Footer">Footer</div>
     </div>
   );
 }
