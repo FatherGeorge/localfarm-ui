ed56dc4fd869c9c891051fe368b4caf6af2b7d0e
fathergeorge <vovathewombat@yandex.ru>
2019-11-22 22:30:32 +0530

    template1

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="App">
       <div className="Header">Header</div>
-      <div className="Search">Search</div>
+      <div className="Search">
+      	<div id="left-col" className="col" style={{"width": "34%"}}>
+      		Left
+      	</div>
+      	<div id="right-col" className="col" style={{"width": "65%"}}>
+      		Right
+      	</div>
+      </div>
       <div className="Footer">Footer</div>
     </div>
   );
