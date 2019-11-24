1399fbeeca96e1ef7b4e0f412df2d07eae640049
fathergeorge <vovathewombat@yandex.ru>
2019-11-24 21:14:51 +0530

    template2

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="Header">Header</div>
+      <div className="Header">
+      	<nav>
+	      	<ul className="right">
+	      		<li>Donate</li>
+	      		<li>Blog</li>
+	      		<li>Find farm</li>
+	      	</ul>
+	    </nav>
+      </div>
       <div className="Search">
       	<div id="left-col" className="col" style={{"width": "34%"}}>
       		Left
